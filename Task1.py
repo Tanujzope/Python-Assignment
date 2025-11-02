c8dd34dfb109d3cec3319cc7062d857883233fb4
Tanujzope <tanujzope5@gmail.com>
2025-11-02 19:42:15 +0530

    First task Complete

---
+++ Task1.py
@@ -0,0 +1,16 @@
+'''Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.'''
+
+inp1 = int(input("Enter the first number: "))
+inp2 = int(input("Enter the Second Number: "))
+
+print("Addition : ", inp1 + inp2)
+print("Substraction: ", inp1 - inp2)
+print("Multiplication: ", inp1 * inp2)
+print("Division: ", inp1 / inp2)
